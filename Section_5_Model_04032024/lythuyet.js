761114c9b9d743b37c3fc5fd26e4ae107c995276
NguyenTrongKhoi04 <nguyentrongkhoi17062004@gmail.com>
2024-03-04 22:38:49 +0700

    up

---
+++ Section_5_Model_04032024/lythuyet.js
@@ -0,0 +1,18 @@
+/**
+ * * "ng-model" dùng để thao tác với các thẻ dữ liệu (input, select, textarea,...)
+ * * tác dụng của ng-model
+ *          validate form $error.(email/required/...)
+ *          kết nối thẻ html với giá trị nhập vào của thẻ input
+ *          status application
+ * * CSS-class by "ng-model"
+ *        ? The ng-model directive adds/removes the following classes, according to the status of the form field:
+            ng-empty
+            ng-not-empty
+            ng-touched
+            ng-untouched
+            ng-valid
+            ng-invalid
+            ng-dirty
+            ng-pending
+            ng-pristine
+ */
