fbc015d176c9958b3e85bec1ff8e7fbfa1bf8568
NguyenTrongKhoi04 <nguyentrongkhoi17062004@gmail.com>
2024-03-05 21:04:25 +0700

    up

---
+++ Section_6_DataBinding_05032024/lythuyet.js
@@ -0,0 +1,8 @@
+/**
+ * ? Data binding in AngularJS is the synchronization between the model and the view.
+ * * ng-controller
+ *      +> Because of the immediate synchronization of the model and the view, the controller can be
+ *              completely separated from the view, and simply concentrate on the model data. Thanks to
+ *              the data binding in AngularJS, the view will reflect any changes made in the controller.
+ *      +> đọc thêm ở https://www.w3schools.com/angular/angular_controllers.asp
+ */
